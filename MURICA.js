7f4c552f31949687c6f1f6d8ef7658b0afac3008
Jacob Pepe <jacob.pepe@gmail.com>
2015-10-10 08:46:42 +0000

    got map showing

--- MURICA.js
+++ MURICA.js
@@ -1,23 +1,32 @@
 if (Meteor.isClient) {
+Template.body.helpers({
+  exampleMapOptions: function() {
+    // Make sure the maps API has loaded
+    if (GoogleMaps.loaded()) {
+      // Map initialization options
+      return {
+        center: new google.maps.LatLng(-37.8136, 144.9631),
+        zoom: 8
+      };
+    }
+  }
+});
 
-    Meteor.startup(function() {
-        GoogleMaps.load();
+Template.body.onCreated(function() {
+  // We can use the `ready` callback to interact with the map API once the map is ready.
+  GoogleMaps.ready('exampleMap', function(map) {
+    // Add a marker to the map once it's ready
+    var marker = new google.maps.Marker({
+      position: map.options.center,
+      map: map.instance
     });
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+  });
+});
+    
+  Meteor.startup(function() {
+    GoogleMaps.load();
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1)
-    }
-  });
 }
 
 if (Meteor.isServer) {
@@ -25,3 +34,5 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
+
